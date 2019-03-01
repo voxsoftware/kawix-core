f25d8a96ebb0dde97c3562c6585007a410761322
James <xtreme.maicolxt94@gmail.com>
2019-03-01 10:30:14 +0000

    Adding cli

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+var file= process.argv[2]
+if(file && !file.startsWith("--")){
+
+    // require file using KModule
+    var Kawix= require("../main")
+    Kawix.KModule.injectImport()
+    Kawix.KModule.import(file,{
+        parent: process.cwd() + "/cli.js"
+    }).then(function(){}).catch(function(e){
+        console.error("Failed executing: ", e)
+    })
+
+}
